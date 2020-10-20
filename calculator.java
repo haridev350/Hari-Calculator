3bf6c8d5324e1f930324054888ccf8d7fc915607
harikr350 <harikr350@gmail.com>
2020-10-20 15:46:49 +0530

    Added Java File

---
+++ calculator.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+public class calculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b,c,d;
+		System.out.println("Enter your choice 1 (ADD) 2 (SUB) 3(MULT) 4 (DIV)");
+		a = sc.nextInt();
+		if (a == 1) {
+			System.out.println("Enter 1st number");
+			b = sc.nextInt();
+			System.out.println("Enter 2nd number");
+			c = sc.nextInt();
+			
+			d= b+c;
+			System.out.println("The answer is " + d);
+		}
+		else if (a == 2) {
+			System.out.println("Enter 1st number");
+			b = sc.nextInt();
+			System.out.println("Enter 2nd number");
+			c = sc.nextInt();
+			
+			d= b-c;
+			System.out.println("The answer is " + d);
+		}
+		else if (a == 3) {
+			System.out.println("Enter 1st number");
+			b = sc.nextInt();
+			System.out.println("Enter 2nd number");
+			c = sc.nextInt();
+			
+			d= b*c;
+			System.out.println("The answer is " + d);
+		}
+		else if (a == 4) {
+			System.out.println("Enter 1st number");
+			b = sc.nextInt();
+			System.out.println("Enter 2nd number");
+			c = sc.nextInt();
+			
+			d= b/c;
+			System.out.println("The answer is " + d);
+		}
+		else {
+			System.out.println("The choice is wrong ! Enter different choice ");
+		}
+	
+		
+
+	}
+
+}
