78edf6312ee3d0622952f8b46e6a2acba7ac0a93
haridev350 <harikr350@gmail.com>
2020-10-20 16:05:41 +0530

    Added new feature

--- calculator.java
+++ calculator.java
@@ -5,7 +5,7 @@ public class calculator {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a,b,c,d;
-		System.out.println("Enter your choice 1 (ADD) 2 (SUB) 3(MULT) 4 (DIV)");
+		System.out.println("Enter your choice 1 (ADD) 2 (SUB) 3(MULT) 4 (DIV) 5 (REMAINDER)");
 		a = sc.nextInt();
 		if (a == 1) {
 			System.out.println("Enter 1st number");
@@ -41,6 +41,15 @@ public class calculator {
 			c = sc.nextInt();
 			
 			d= b/c;
+			System.out.println("The answer is " + d);
+		}
+		else if (a == 5) {
+			System.out.println("Enter 1st number");
+			b = sc.nextInt();
+			System.out.println("Enter 2nd number");
+			c = sc.nextInt();
+			
+			d= b%5c;
 			System.out.println("The answer is " + d);
 		}
 		else {
